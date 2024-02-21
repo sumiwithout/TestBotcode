e7454cc5efe2a56b61f83c38662ba9f5353153d6
Rei <remwasslacking@gmail.com>
2024-02-21 00:37:04 -0600

    git commit

---
+++ src/main/java/frc/robot/commands/ArmUp.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LauncherSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+public class ArmUp extends Command{
+    private Timer m_timer;
+    ArmSubsystem m_arm = ArmSubsystem.getInstance();
+    LauncherSubsystem m_launcher = LauncherSubsystem.getInstance();
+    // help
+    public ArmUp(){
+      
+        addRequirements(m_arm);
+    }
+    @Override
+          public void initialize() {            
+          }
+
+          @Override
+          public void execute() {
+            m_arm.setTargetPosition(Constants.Arm.kScoringPosition);
+            m_arm.runAutomatic();
+          }
+
+          @Override
+          public boolean isFinished() {
+         return   m_arm.isNearTarget();
+          }
+
+          @Override
+          public void end(boolean interrupted) {
+          }
+}
